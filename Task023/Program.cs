9a742eb82cfd3ad173149fec3f2c0a09e1ba306c
DVZ <orionvega5@gmail.com>
2022-08-13 22:38:16 +0300

    Add HomeTask 023

---
+++ Task023/Program.cs
@@ -0,0 +1,14 @@
+﻿void PowerN3(int num)
+{
+    int index = 1;
+    while (index < num)
+    {
+        Console.Write($"{Math.Pow(index, 3)}, ");
+        index++;
+    }
+    Console.Write($"{Math.Pow(index, 3)}. ");
+}
+
+Console.Write("Введите любое целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+PowerN3(num);
